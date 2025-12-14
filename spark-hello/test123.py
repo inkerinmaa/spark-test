4dace97b7c24d2e9f373103c0e06a164c0295988
Nikita Gladkiy <nikita.gladkiy@rwl.ru>
2025-12-14 19:31:18 +0300

    Initial commit5

--- spark-hello/test123.py
+++ spark-hello/test123.py
@@ -1,34 +1,26 @@
-# simple_tcp_streaming.py
-import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: simple_tcp_streaming.py <host> <port>")
-        sys.exit(1)
-    
-    host = sys.argv[1]
-    port = sys.argv[2]
-    
-    spark = SparkSession.builder \
-        .appName("SimpleTCPStream") \
-        .getOrCreate()
-    
-    spark.sparkContext.setLogLevel("WARN")
-    
-    # Read from socket
-    lines = spark.readStream \
-        .format("socket") \
-        .option("host", host) \
-        .option("port", port) \
-        .load()
-    
-    # Print raw messages
-    print("Starting to listen for messages...")
-    
-    query = lines.writeStream \
-        .outputMode("append") \
-        .format("console") \
-        .start()
-    
-    query.awaitTermination()
+spark = SparkSession \
+    .builder \
+    .appName("StructuredNetworkWordCount") \
+    .getOrCreate()
+
+# Create DataFrame representing the stream of input lines from connection to localhost:9999
+lines = spark \
+    .readStream \
+    .format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load()
+
+# Split the lines into words
+words = lines.select(
+   explode(
+       split(lines.value, " ")
+   ).alias("word")
+)
+
+# Generate running word count
+wordCounts = words.groupBy("word").count()
