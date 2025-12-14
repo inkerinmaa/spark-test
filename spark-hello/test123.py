672d131f8400818b9e41460adc9423a8be477e98
Nikita Gladkiy <nikita.gladkiy@rwl.ru>
2025-12-14 18:16:15 +0300

    Initial commit3

--- spark-hello/test123.py
+++ spark-hello/test123.py
@@ -1,13 +1,28 @@
+import sys
+from random import random
+from operator import add
+
 from pyspark.sql import SparkSession
 
+
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("hello-world").getOrCreate()
+    """
+        Usage: pi [partitions]
+    """
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPi")\
+        .getOrCreate()
 
-    print("Hello from PySpark running on Kubernetes via Spark Operator!")
-    print("Spark version:", spark.version)
+    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    n = 100000 * partitions
 
-    # tiny action to prove the job actually ran
-    n = spark.range(1, 6).count()
-    print("Count(range(1,6)) =", n)
+    def f(_: int) -> float:
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    print("Pi is roughly %f" % (4.0 * count / n))
 
     spark.stop()
